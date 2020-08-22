279c2f206cf4304c6c1216bdfe4b337eea1640d0
Xkonti <online.xkonti@gmail.com>
2020-08-22 04:49:14 -0400

    Document the Common module
    Relates to #1
    The common module doesn't have any description itself as something isn't working right... Issue: christopherthielen/typedoc-plugin-external-module-name#515

--- src/common/number-helpers.ts
+++ src/common/number-helpers.ts
@@ -12,7 +12,14 @@ export function randomFloatPositive(): number {
 }
 
 /**
- * Returns a random integer between min and max values. Min and max are inclusive.
+ * Returns a random integer between `min` and `max` values. Both `min` and `max`
+ * are inclusive.
+ * @example
+ * ```ts
+ * import { randomIntRange } from '@hubhazard/core';
+ * const x = randomIntRange(1, 5);
+ * // Possible x values: 1, 2, 3, 4, 5
+ * ```
  */
 export function randomIntRange(min: number, max: number): number {
   min = Math.round(min);
