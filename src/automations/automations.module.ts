3852e5838f25c2f35544c188e1e4d01de7e56db5
Xkonti <online.xkonti@gmail.com>
2020-08-22 04:50:48 -0400

    Add description to the Automations module
    Related to #1

--- src/automations/automations.module.ts
+++ src/automations/automations.module.ts
@@ -1,6 +1,27 @@
 /**
+ * You can consider this module to be the core of the HubHazard Core. The
+ * {@link AutomationsModule} is responsible for providing base classes and
+ * interfaces for events services, events, automations and is responsible for
+ * managing registered event services and automations:
+ *
+ * - {@link Automation} - a base class for every automation. Allows for
+ *      automations to be correctly registered and triggered.
+ * - {@link AutomationEvent} - base class for events passed to automations.
+ *      Every automation event provides the `eventType` string field that allows
+ *      for identification of the event type inside the automation.
+ * - {@link AutomationsService} - a service for registering automations and
+ *      events services.
+ * - {@link BuildableToTriggerDefinition} - a base class for classes used in
+ *      describing triggers. This allows them to be *buildable* to the
+ *      {@link TriggerDefinition} consumed by events services.
+ * - {@link IEventsService} - an interface describing basic functionality of the
+ *      event service: registering and unregistering automations.
+ * - {@link TriggerDefinition} - a base class for trigger definitions. Trigger
+ *      definitions describe what kind of events an automation awaits.
+ *
  * @packageDocumentation
  * @module Automations
+ * @preferred
  */
 
 import { Module } from '@nestjs/common';
