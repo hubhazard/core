d65a5ca52c085f6dd8e33879ceadc829a6c55df0
Xkonti <online.xkonti@gmail.com>
2020-08-23 03:01:03 -0400

    Add the documentation of Automations module
    Related to #7

--- src/automations/automation.ts
+++ src/automations/automation.ts
@@ -9,7 +9,8 @@ import { BuildableToTriggerDefinition } from './buildable-to-trigger-definition'
 import { TriggerDefinition } from './trigger-definition';
 
 /**
- * A base class for all automations.
+ * A base class for all automations. Allows for automations to be correctly
+ * registered and triggered.
  */
 @Injectable()
 export abstract class Automation {
@@ -19,7 +20,14 @@ export abstract class Automation {
   abstract readonly name: string;
 
   /**
-   * A list of not-yet-built triggers. Empty list means this automation won't be triggered.
+   * A list of not-yet-built triggers. Empty list means this automation won't be
+   * triggered.
+   *
+   * @example
+   * ```ts
+   * // List of triggers this automation subscribes to.
+   * readonly triggers = [TimerTrigger.every(10, 'seconds')];
+   * ```
    */
   abstract readonly triggers: BuildableToTriggerDefinition[];
 
@@ -30,8 +38,9 @@ export abstract class Automation {
   private builtTriggersCache: TriggerDefinition[] | undefined = undefined;
 
   /**
-   * Returns a list of triggers already built as `TriggerDefinition`. This value
-   * is cached.
+   * Returns a list of triggers already built as {@link TriggerDefinition}.
+   * This value is cached. This is used by {@link IEventsService events services}.
+   * @internal
    */
   get builtTriggers(): TriggerDefinition[] {
     if (this.builtTriggersCache == null) {
@@ -41,7 +50,9 @@ export abstract class Automation {
   }
 
   /**
-   * A function called upon event matching automations triggers.
+   * A function called upon event matching automations triggers. This function
+   * is called by {@link IEventsService events services}.
+   * @internal
    */
   abstract async handleEvent(automationEvent: AutomationEvent): Promise<void>;
 }
